bd64ebc4ecbb895bf7fb83f2de50c72c7850382d
Arti <arti@infrrd.ai>
2018-10-02 12:00:00 +0530

    new java code is added

---
+++ src/main/java/ai/infrrd/controller/TestController.java
@@ -0,0 +1,16 @@
+package ai.infrrd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+	
+	@GetMapping(value="/hello")
+	public String wishMess() {
+		return "hello Arti";
+	}
+
+}
